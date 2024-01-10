8051a67d905d0acc84deb8f1bdf8d6a865d575ba
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2024-01-10 23:39:56 +0530

    added my select options

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,71 @@
 function Home() {
-  return <div>Home Page</div>;
+  // Informative Frontend content and to know trends in context with
+  //               software engineering and to learn in public for better future opprtunities
+
+  return (
+    <div>
+      <h3>Technologies that take control over me or use me sometimes:</h3>
+      <div style={{ display: "flex" }}>
+        <ul style={{ listStyle: "none" }}>
+          <li>Youtube</li>
+          <li>Youtube Music</li>
+          <li>LinkedIn</li>
+        </ul>
+        <ul style={{ listStyle: "none" }}>
+          <li>
+            <select value={"FOMO"}>
+              <option value={""}>
+                {"Select a why you can't stop using this tech"}
+              </option>
+              <option value={"FOMO"}>
+                Fear Of Missing Out in Frontend/other things
+              </option>
+              <option value={"Easy escape from reality and you feel good"}>
+                You feel good and Easy escape from reality
+              </option>
+              <option value={"learn in public"}>
+                Learn in public for better future opprtunities but you scroll
+              </option>
+            </select>
+          </li>
+          <li>
+            {" "}
+            <select value={"Easy escape from reality and you feel good"}>
+              <option value={""}>
+                {"Select a why you can't stop using this tech"}
+              </option>
+              <option value={"FOMO"}>
+                Fear Of Missing Out in Frontend/other things
+              </option>
+              <option value={"Easy escape from reality and you feel good"}>
+                You feel good and Easy escape from reality
+              </option>
+              <option value={"learn in public"}>
+                Learn in public for better future opprtunities but you scroll
+              </option>
+            </select>
+          </li>
+          <li>
+            {" "}
+            <select value={"learn in public"}>
+              <option value={""}>
+                {"Select a why you can't stop using this tech"}
+              </option>
+              <option value={"FOMO"}>
+                Fear Of Missing Out in Frontend/other things
+              </option>
+              <option value={"Easy escape from reality and you feel good"}>
+                You feel good and Easy escape from reality
+              </option>
+              <option value={"learn in public"}>
+                Learn in public for better future opprtunities but you scroll
+              </option>
+            </select>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
