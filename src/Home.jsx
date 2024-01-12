360fe2dd14e4667ea486f9ef944096c08b3eda56
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2024-01-12 17:28:07 +0530

    tailwind setup and sorting extension installed

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,8 @@
 function Home() {
   return (
     <div>
-      <p>Cue | Craving | Response | Reward</p>
+      {/* <p>Cue | Craving | Response | Reward</p> */}
+      <h1 className='text-3xl font-bold underline'>Hello d world!</h1>
     </div>
   );
 }
